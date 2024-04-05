3563b2ea2da5a2831cf5ef74e05cf8812143f806
pritishmd <159415077+pritishmd@users.noreply.github.com>
2024-04-05 12:38:37 +0530

    Set Up Prometheus & Grafana on Local with the help of docker compose

---
+++ src/main/java/com/pritish/observability/BootProj04OtelGrafanaPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.pritish.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj04OtelGrafanaPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj04OtelGrafanaPrometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pritish/observability/BootProj03OtelGrafanaPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritish.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj03OtelGrafanaPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
